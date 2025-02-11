d1cb15ea57dfb458c59c67aa1ffd7358b8028493
Boyne <boynegregg312@gmail.com>
2025-02-10 20:58:30 -0500

    Applied speed modifiers for elevator heights

--- src/main/java/frc/robot/Subsystems/SwerveDrive/DriveCommandFactory.java
+++ src/main/java/frc/robot/Subsystems/SwerveDrive/DriveCommandFactory.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Commands.SwerveDriveCommands.FieldOrientedDriveCommand;
 import frc.robot.Commands.SwerveDriveCommands.LockSwerves;
+import frc.robot.Subsystems.Elevator.ElevatorSubsystem;
 import frc.robot.Utils.JoystickUtils.ControllerInterface;
 import frc.robot.Utils.SwerveDriveUtils.SwerveDriveRotationProfiler;
 import frc.robot.Utils.SwerveDriveUtils.SwerveDriveTranslationProfiler;
@@ -11,18 +12,21 @@ import frc.robot.Utils.SwerveDriveUtils.SwerveDriveTranslationProfiler;
 public class DriveCommandFactory {
     // All Subsystems needed for every command
     DriveSubsystem driveSubsystem;
+    ElevatorSubsystem elevatorSubsystem;
 
     // All other dependencies for every command that uses primarly DriveSubsystem
     ControllerInterface driverController;
 
-    public DriveCommandFactory(DriveSubsystem driveSubsystem, ControllerInterface driverController) {
+    public DriveCommandFactory(DriveSubsystem driveSubsystem, ElevatorSubsystem elevatorSubsystem, ControllerInterface driverController) {
         this.driveSubsystem = driveSubsystem;
+        this.elevatorSubsystem = elevatorSubsystem;
         this.driverController = driverController;
     }
 
     public FieldOrientedDriveCommand createFieldOrientedDriveCommand() {
         return new FieldOrientedDriveCommand(
         this.driveSubsystem, 
+        this.elevatorSubsystem,
         () -> -this.driverController.getLeftY(),
         () -> -this.driverController.getLeftX(),
         () -> -this.driverController.getRightX());
