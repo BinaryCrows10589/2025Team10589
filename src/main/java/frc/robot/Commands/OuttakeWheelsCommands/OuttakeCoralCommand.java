ed99cefc25e9d78de6e1e3e020c4d2b571a3e5c5
Jetvac2 <jetvac2@icloud.com>
2025-03-08 23:24:10 -0500

    Variable Outtake voltage bassed on height

--- src/main/java/frc/robot/Commands/OuttakeWheelsCommands/OuttakeCoralCommand.java
+++ src/main/java/frc/robot/Commands/OuttakeWheelsCommands/OuttakeCoralCommand.java
@@ -7,6 +7,8 @@ import com.ctre.phoenix6.controls.jni.ControlConfigJNI;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.GenericConstants.ControlConstants;
 import frc.robot.Constants.MechanismConstants.OuttakeConstants;
+import frc.robot.Subsystems.Elevator.ElevatorSubsystem;
+import frc.robot.Subsystems.Elevator.ElevatorSubsystem.ElevatorPosition;
 import frc.robot.Subsystems.Outtake.OuttakeCoralSensors.OuttakeCoralSensorsIO;
 import frc.robot.Subsystems.Outtake.OuttakeCoralSensors.OuttakeCoralSensorsSubsystem;
 import frc.robot.Subsystems.Outtake.OuttakeWheels.OuttakeWheelsSubsystem;
@@ -36,7 +38,15 @@ public class OuttakeCoralCommand extends Command{
     @Override
     public void initialize() {  
         this.hardCutOffTimer.startTimer();
-        this.outtakeWheelsSubsystem.setWheelVoltages(OuttakeConstants.kRightWheelOuttake);
+        if(ControlConstants.desiredElevatorPosition == ElevatorSubsystem.resolveElevatorPosition(ElevatorPosition.L4)) {
+            this.outtakeWheelsSubsystem.setWheelVoltages(OuttakeConstants.kRightWheelOuttakeL4);
+        } else if(ControlConstants.desiredElevatorPosition == ElevatorSubsystem.resolveElevatorPosition(ElevatorPosition.L3)){
+            this.outtakeWheelsSubsystem.setWheelVoltages(OuttakeConstants.kRightWheelOuttakeL3);
+        } else if(ControlConstants.desiredElevatorPosition == ElevatorSubsystem.resolveElevatorPosition(ElevatorPosition.L2)){
+            this.outtakeWheelsSubsystem.setWheelVoltages(OuttakeConstants.kRightWheelOuttakeL2);
+        } else {
+            this.outtakeWheelsSubsystem.setWheelVoltages(OuttakeConstants.kRightWheelOuttake);
+        }
         Logger.recordOutput("Outtake/IsRunningOuttakeCommand", true);
     }
 
